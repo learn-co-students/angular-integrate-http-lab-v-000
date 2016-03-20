f30fc7398ad21b18e3717e1f90125172e2cc0d0c
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-03-20 11:13:13 -0400

    Done

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,16 @@
 function UserService($http) {
 
+	this.getUser = function(){
+		return $http.get('http://0.0.0.0:8882/rest/user')
+		// .then(function(data) {
+		// 	console.log(data);
+		// });
+	}
+
+	this.updateUser = function(newUser){
+		return $http.post('http://0.0.0.0:8882/rest/user', newUser)
+	}
+
 }
 
 angular
