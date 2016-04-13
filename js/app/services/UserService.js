6cf38b1411fb9c4ce853671d2e389418ac55366a
empireofryan <rjohnson2001@gmail.com>
2016-04-13 16:35:06 -0400

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,7 +1,15 @@
 function UserService($http) {
+  this.getUser = function () {
+    return $http.get('http://0.0.0.0:8882/rest/user');
+  };
+
+  this.updateUser = function (userInfo) {
+    return $http.post('http://0.0.0.0:8882/rest/user', userInfo);
+  };
+
 
 }
 
 angular
-	.module('app')
-	.service('UserService', UserService);
+  .module('app')
+  .service('UserService', UserService);
