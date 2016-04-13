18086cb42ac94246a80b04629d44374a18f7c0b1
Rawradin <edgar.rawr@gmail.com>
2016-04-13 09:38:52 -0700

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,7 +1,14 @@
 function UserService($http) {
+    this.getUser = function () {
+      return $http.get('http://0.0.0.0:8882/rest/user');
+    };
 
+    this.updateUser = function () {
+    	data = {'first_name': 'Edgar', 'last_name': 'Gonzalez', 'email': 'eddiiiieeee@gmail.com'};
+    	return $http.post('http://0.0.0.0:8882/rest/user',data);
+    };
 }
-
+ 
 angular
-	.module('app')
-	.service('UserService', UserService);
+    .module('app')
+    .service('UserService', UserService);
