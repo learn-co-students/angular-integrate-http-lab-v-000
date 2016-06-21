cebd4e280aa12043ba80c745d243b9bdc5e19715
the-widget <brennenawana108ny@gmail.com>
2016-06-21 11:05:14 -0400

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,6 +1,14 @@
 function UserService($http) {
 
-}
+  this.getUser = function(){
+    return $http.get('http://0.0.0.0:8882/rest/user');
+  };
+
+  this.updateUser = function(userData){
+    return $http.post('http://0.0.0.0:8882/rest/user', userData);
+  };
+
+};
 
 angular
 	.module('app')
