b4a42be8d2e4cfb172f7a4e0c25d92ce20770e63
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-06-23 17:52:49 -0500

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,12 @@
 function UserService($http) {
 
+  this.getUser = function(){
+		return $http.get("http://0.0.0.0:8882/rest/user");
+	};
+
+	this.updateUser = function(userData){
+		return $http.post("http://0.0.0.0:8882/rest/user", userData);
+	};
 }
 
 angular
