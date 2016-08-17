1663529c90efe0b59d2a24ac0eec0aee697cf5c9
Ziv Zamechek <ziv921@yahoo.com>
2016-08-17 09:49:12 -0400

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,7 +1,16 @@
 function UserService($http) {
 
+  this.getUser = function () {
+        return $http.get('http://0.0.0.0:8882/rest/user');
+    };
+
+    this.updateUser = function (info) {
+
+      return $http.post('http://0.0.0.0:8882/rest/user', {info: info});
+    };
+
 }
 
 angular
-	.module('app')
-	.service('UserService', UserService);
+  .module('app')
+  .service('UserService', UserService);
