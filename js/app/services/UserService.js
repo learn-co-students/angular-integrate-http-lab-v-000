5dac3d4a62e3e7419d649022431c8f2d03b792c0
Savannah Scott <sascott624@gmail.com>
2016-08-19 12:10:00 -0400

    Done.

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,4 +1,11 @@
 function UserService($http) {
+	this.getUser = function(){
+		return $http.get('http://0.0.0.0:8882/rest/user');
+	}
+
+	this.updateUser = function(firstName, lastName, emailAddress){
+		return $http.post('http://0.0.0.0:8882/rest/user', {first_name: firstName, last_name: lastName, email: emailAddress} )
+	}
 
 }
 
