0993fbc6ee20e1584b2ff4c2ea1016ee0045005e
agdavid <antonio.g.david@gmail.com>
2016-08-30 13:56:19 -0400

    create http get method in custom service

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,9 @@
 function UserService($http) {
-
+  // http get request
+  this.getUser = function() {
+    var response = $http.get('http://0.0.0.0:8882/rest/user');
+    return response
+  };
 }
 
 angular
