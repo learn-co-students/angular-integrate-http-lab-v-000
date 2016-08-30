72500fc2f1f7958caf0d9f66cc1f2dfe37dab8cc
agdavid <antonio.g.david@gmail.com>
2016-08-30 13:58:43 -0400

    create http post request for custom service

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -4,6 +4,12 @@ function UserService($http) {
     var response = $http.get('http://0.0.0.0:8882/rest/user');
     return response
   };
+
+  // http post request
+  this.updateUser = function(data) {
+    var response = $http.post('http://0.0.0.0:8882/rest/user', data);
+    return response
+  }
 }
 
 angular
