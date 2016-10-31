30622bd69dae71524500cc54b4a9a29aecbca1aa
jd2rogers2 <jd2rogers2@gmail.com>
2016-10-31 10:20:42 -0400

    donzo

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,18 @@
 function UserService($http) {
-
+  var serv = this;
+  serv.getUser = function(){
+    return $http({
+      method: 'GET',
+      url: 'http://0.0.0.0:8882/rest/user'
+    })
+  }
+  serv.updateUser = function(new_email){
+    return $http({
+      method: 'POST',
+      url: 'http://0.0.0.0:8882/rest/user',
+      data: {email: new_email}      
+    })
+  }
 }
 
 angular
