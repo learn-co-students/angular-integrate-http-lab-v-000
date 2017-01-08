286dda564d96acf9c638aefcf03496177d226dad
Chris Aporta <christopher.aporta@gmail.com>
2017-01-07 23:53:46 -0500

    complete lab

--- js/app/services/UserService.js
+++ js/app/services/UserService.js
@@ -1,5 +1,13 @@
 function UserService($http) {
+  const baseUrl = `http://0.0.0.0:8882`
 
+  this.getUser = () => {
+    return $http.get(`${baseUrl}/rest/user`)
+  }
+
+  this.updateUser = () => {
+    return $http.post(`${baseUrl}/rest/user`, {})
+  }
 }
 
 angular
